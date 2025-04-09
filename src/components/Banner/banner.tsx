56adb0106b55bf5f6be151c53d1525df61cda506
Carlos Henrique dos Santos <129197782+CarlosCafeJs@users.noreply.github.com>
2025-04-09 00:01:44 +0000

    Alterações de controle.

--- src/components/Banner/banner.tsx
+++ src/components/Banner/banner.tsx
@@ -7,17 +7,17 @@ import { kameron, libre } from '@/app/fonts'
 
 const Banner: React.FC = () => {
   return (
-    <div className="w-full  grid grid-cols-3 gap-4 flex items-center justify-center">
-      <div className='col-span-2 p-24'>
+    <div className="w-full grid grid-cols-12 gap-4 items-center mx-auto">
+      <div className='col-span-8 p-24  bg-black' >
         <div className='gap-y-1.5'>
           <h3 className={`${kameron.className} text-center text-4xl text-amber-950`}>O Poder de uma pele</h3>
-          <h1 className={`${libre.className} text-8xl p-2 text-amber-50`}>Bem Cuidada</h1>
+          <h1 className={`${libre.className} text-8xl p-2  text-amber-50`}>Bem Cuidada</h1>
           <h3 className={`${kameron.className} text-end text-4xl text-amber-950`}>  Começa aqui!</h3>
 
           <div className='py-12 z-40 flex items-center justify-center gap-8 text-2xl'>
             <Link href="/" ><button className="flex my-8 flex items-center bg-amber-950 text-white   py-1 rounded-full w-74 justify-evenly">
               <span className={`${kameron.className} text-end text-2xl`}>Entrar em contato</span>
-              <span className=" rounded-full bg-amber-950">
+              <span className="animate-bounce rounded-full bg-amber-950">
                 <Image
                   src="/imgArrowContat.svg"
                   alt="Arrow Contat .img"
@@ -32,7 +32,7 @@ const Banner: React.FC = () => {
           </div>
         </div>
       </div>
-      <div className='my-8'>
+      <div className='my-8 col-span-4 '>
         <Image
           src="/imgBodyApresent.svg"
           alt="banner .img"
